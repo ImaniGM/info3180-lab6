4090ca34bc85fed1af24724d18e9b6ac84109d25
ImaniGM <imani.miller24@gmail.com>
2021-04-04 21:27:44 -0500

    lab6done

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -1,11 +1,103 @@
 /* Add your Application JavaScript */
+const Home = {
+  name: 'Home',
+  template: `
+  <div class="home">
+  <img src="/static/images/logo.png" alt="VueJS Logo">
+  <h1>{{ welcome }}</h1>
+  </div>
+  `,
+  data() {
+      return {
+          welcome: 'Hello Imani! Welcome to VueJS'
+      }
+  }
+};
+
+
+const NewsList = {
+  name: 'NewsList',
+  template: `
+    <div id = "temp" class="form-inline d-flex justify-content-center">
+      <div class="form-group mx-sm-3 mb-2">
+      <label class="sr-only" for="search">Search</label>
+      <input type="search" name="search" v-model="searchTerm"id="search" class="form-control mb-2 mr-sm-2" placeholder="Enter search term here" />
+      <button class="btn btn-primary mb-2" @click="searchNews">Search</button>
+  </div>
+    <p>You are searching for {{ searchTerm }}</p>
+    <div class="news">
+    <h2>News</h2>
+    <ul class="news__list">
+        <li v-for="article in articles" class="news__item">
+        <b>{{ article.title }}</b>
+        <br>
+        <img class="photo" v-bind:src="article.urlToImage"/>
+        <br>
+        {{ article.description }}
+        <br>
+        <br>
+      </li>
+    </ul>
+  </div>
+  </div>
+  `,
+  created() {
+      let self = this;
+
+      fetch('https://newsapi.org/v2/top-headlines?country=us', {
+              headers: {
+                'Authorization': 'Bearer 2f3a1ea889d74673ae919cb3bf2fe5e1'
+              }
+          })
+          .then(function(response) {
+              return response.json();
+          })
+          .then(function(data) {
+              console.log(data);
+              self.articles = data.articles;
+          });
+  },
+  data() {
+      return {
+          articles: [],
+          searchTerm: ''
+      }
+  },
+  methods: {
+      searchNews() {
+          let self = this;
+          fetch('https://newsapi.org/v2/everything?q=' +
+                  self.searchTerm + '&language=en', {
+                      headers: {
+                          'Authorization': 'Bearer 2f3a1ea889d74673ae919cb3bf2fe5e1'
+                      }
+                  })
+              .then(function(response) {
+                  return response.json();
+              })
+              .then(function(data) {
+                  console.log(data);
+                  self.articles = data.articles;
+              });
+      }
+  }
+  };
+
+
 const app = Vue.createApp({
   data() {
     return {
-      welcome: 'Hello World! Welcome to VueJS'
+      welcome: 'Hello Imani! Welcome to VueJS'
     }
-  }
+  },
+    components: {
+      'home': Home,
+      'news-list': NewsList
+
+    }
+  
 });
+
 
 app.component('app-header', {
   name: 'AppHeader',
@@ -20,10 +112,10 @@ app.component('app-header', {
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
               <ul class="navbar-nav mr-auto">
                 <li class="nav-item active">
-                  <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                <router-link to="/" class="nav-link">Home</router-link>
                 </li>
                 <li class="nav-item">
-                  <a class="nav-link" href="#">News</a>
+                <router-link to="/news" class="nav-link">News</router-link>
                 </li>
               </ul>
             </div>
@@ -51,4 +143,13 @@ app.component('app-footer', {
   }
 })
 
+const router = VueRouter.createRouter({
+  history: VueRouter.createWebHistory(),
+  routes: [
+      { path: '/', component: Home },
+      { path: '/news', component: NewsList }
+  ]
+});
+
+app.use(router)
 app.mount('#app');
